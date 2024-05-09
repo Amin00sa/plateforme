7fa11807011336994553300b6d5d3fd06a66ab37
Amin00sa <ami0190ne@gmail.com>
2024-05-09 17:48:34 +0100

    Fixing header height

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="sticky top-0 z-50" :class="isTopPage?'bg-black':'bg-white'">
-    <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+    <nav class="mx-auto flex max-w-7xl items-center justify-between p-[0.7rem] lg:px-8" aria-label="Global">
       <div class="flex lg:flex-1">
         <RouterLink to="/" class="-m-1.5 p-1.5">
           <span class="sr-only">Your Company</span>
